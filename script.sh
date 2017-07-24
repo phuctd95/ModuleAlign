e2caa2c1fe1429706c0c27624318bc902af85103
Thai Dinh Phuc <conan.doyle.1995@gmail.com>
2017-07-24 18:17:03 +0700

    change

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
+awk 'NR==FNR{a[$2]=$1;next}($1 in a){print a[$1]"\t"$2"\t"$3}' Node/$1.node $1-$2.blast > temp-1 
+awk 'NR==FNR{a[$2]=$1;next}($2 in a){print $1"\t"a[$2]"\t"$3}' Node/$2.node temp-1 > blastNum
+rm temp-1
+
 ./makeScore $1 $2 blastNum simFile
 ./align $1 $2 simFile -a $3
